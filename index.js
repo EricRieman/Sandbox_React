547a7470427972629d29a119280a8e2679c8fce7
Eric Rieman <er28@zips.uakron.edu>
2021-05-12 13:17:48 -0400

    Hello world with react and jsx

---
+++ index.js
@@ -0,0 +1,9 @@
+// ReactDOM and JSX
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+ReactDOM.render( <h1>Hello world</h1> document.getElementById("root" ); // paramerter: what react will render, where to put it
+
+// JSX allows us to pur html code in our javascript. In order for that to work, you must import react                
