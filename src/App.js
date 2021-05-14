93617a94fda79a1a46aa0cc23b999ec6ea9c84a4
eric.rieman <eric.rieman@appliedvision.com>
2021-05-14 17:17:15 -0400

    still setting up styled todo list

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ToDoItem from "./ToDoItem"
+
+function App()
+{
+    return(
+        <div>
+            <ToDoItem />
+
+            <ToDoItem />
+
+            <ToDoItem />
+        </div>
+    )
+}
+
+export default App;
