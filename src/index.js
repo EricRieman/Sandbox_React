eb896f8a8c946f4ce493d5257378069bf1b63865
eric.rieman <eric.rieman@appliedvision.com>
2021-05-14 13:25:23 -0400

    Styling react components with css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-// Creating a ToDo app by making an App coponent render 3 check boxes with 
-// paragraphs next to them
+// when styling in jsx, we cannot use the typial 'class' for defining 
+// css classes, instead we use 'className'
+// we cannot apply a className to a react component, only its internal html elements
 
 import ReactDOM from "react-dom"
-import App from "./App"
+import App from "./style.css"
 
-ReactDOM.render( <App />, document.getElementById("root") );
+const Test = () => <nav className = "navBar">Hello</nav>
+
+ReactDOM.render( <Test />, document.getElementById("root") );
