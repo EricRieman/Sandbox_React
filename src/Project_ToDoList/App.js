57f5465436b51e127c0bac9240ebd9dc85a8a386
Eric Rieman <er28@zips.uakron.edu>
2021-05-20 23:19:25 -0400

    Added state to todo list project

--- src/Project_ToDoList/App.js
+++ src/Project_ToDoList/App.js
@@ -1,21 +1,26 @@
-// Note that this code will produce a warning about assigning the checked propery of the checkbox.
-// The checkboxes will no longer work as this removs the button handler.
-// This is expected, we will learn how to clean it up later.
+// convert into a class component, load the todoData into a state
+import React from "react"
 
 import ToDoItem from "./ToDoItem"
 import todoData from "./todoData"
 
 import "./style.css"
 
-function App()
-{
-    const todoComponents = todoData.map( data => <ToDoItem key={data.id} todo={data} /> );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { todos: todoData }
+    }
+    
+    render() {
+        const todoComponents = this.state.todos.map( data => <ToDoItem key={data.id} todo={data} /> );
 
-    return(
-        <div className ="todo-list">
-            {todoComponents}
-        </div>
-    )
+        return(
+            <div className ="todo-list">
+                {todoComponents}
+            </div>
+        )
+    }
 }
 
 export default App;
