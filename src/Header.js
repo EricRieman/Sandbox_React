d06bac079834308565eea1c8a39962687cc40364
eric.rieman <eric.rieman@appliedvision.com>
2021-05-24 17:01:16 -0400

    Task4: Meme generator fetches images from API

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header>
-            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="problem loading image"/>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="problem loading"/>
             <p>Meme Generator</p>
         </header>
     )
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,5 +1,6 @@
 // 3) initialize state to save the following data: top text, bottom text, random image from http://i.imgflip.com/1bij.jpg
-
+// 4) make an api call to http://api.imgflip.com/get_memes and save the data that comes back,
+//    (response.data.memes) to a new state property called 'allMemeImgs' (which is an array)
 import React from "react"
 
 class MemeGenerator extends React.Component {
@@ -8,14 +9,29 @@ class MemeGenerator extends React.Component {
         this.state = {
             topText: "",
             bottomText: "",
-            randomImg: "http://i.imgflip.com/1bij.jpg"
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
     }
+
+    componentDidMount() {
+        fetch( "https://api.imgflip.com/get_memes" )
+            .then( response => response.json())
+            .then( response => {
+                const memes = response.data
+                console.log(memes)
+                this.setState({allMemeImgs: memes})
+            })
+    }
+
 
     render() {
         return (
             <div>
-                Testing
+                <form className= "meme-form">
+                    <button>Generate</button>
+
+                </form>
             </div>
         )
     }
