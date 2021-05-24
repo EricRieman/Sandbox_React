d461d38de7d64e3109c2afbbc72797bb9dae02e2
eric.rieman <eric.rieman@appliedvision.com>
2021-05-24 16:46:15 -0400

    Task3: Initialize state of meme generator

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,9 +1,15 @@
+// 3) initialize state to save the following data: top text, bottom text, random image from http://i.imgflip.com/1bij.jpg
+
 import React from "react"
 
 class MemeGenerator extends React.Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg"
+        }
     }
 
     render() {
