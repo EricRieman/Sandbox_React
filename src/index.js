64e13bdb45c8f987ffc56cfe8eab8f9d53799bd6
eric.rieman <eric.rieman@appliedvision.com>
2021-05-26 11:58:58 -0400

    Learning about different ways to create components

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDom from "react-dom"
 
 import App from "./App"
 
+
+// We can also create components using React's createElement method.
+const Greating = () => {
+    return React.createElement( 'h1', {}, 'hello world')
+}
+
+// We can also next the createElement calls for a hierachy
+const Greatting2 =() => {
+    return React.createElement( 'div', {}, React.createElement( 'h1', {}, 'hello world'))
+}
+
 ReactDom.render( <App />, document.getElementById("root") )
+ReactDom.render( <Greating />, document.getElementById("test1") )
+ReactDom.render( <Greating2 />, document.getElementById("test1") )
