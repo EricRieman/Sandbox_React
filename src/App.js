b9ee660772c761daaf8e106f5379dde3b3ef3b9d
eric.rieman <eric.rieman@appliedvision.com>
2021-05-27 17:21:43 -0400

    Learned about importing and exporting.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,11 @@ import React from "react"
 
 import "./app.css"
 
-function App() {
-    const books = [
-        {
-            img: "https://images-na.ssl-images-amazon.com/images/I/61pJRtxpm3L._AC_UL210_SR210,210_.jpg",
-            title: "Groundwater Science",
-            author: "Charles R. Fitts",
-            key: 0
-        },
-        {
-            img: "https://images-na.ssl-images-amazon.com/images/I/51qQ7tba6-L._SX382_BO1,204,203,200_.jpg",
-            title: "Fundamentals of Hydrology",
-            author: "Tim Davie",
-            key: 1
-        }
-    ]
+import books from "./Books"
+import Book from "./Book"
 
-    const mappedBooks = books.map( (book) => <Book data={book} key={book.key}/> )
+function App() {
+    const mappedBooks = books.map( (book) => <Book {...book} key={book.id}/> )
 
     return (
         <div className = "book-list" >
@@ -27,41 +15,4 @@ function App() {
     )
 }
 
-const Book = (props) => {
-    return (
-    <div className = "book">
-        <Image img = {props.data.img}/>
-        <Title title = {props.data.title} />
-        <Author author = {props.data.author} />
-    </div>
-    )
-}
-
-const Image = ({img}) => {
-    return (
-        <img src={img} alt=" " />
-    )
-}
-
-const Title = ({title}) => {
-    return (
-        <h1>{title}</h1>
-    )
-}
-
-// Note that the JSX styling below will always overwrite whatever is in the .css file,
-// if both style types are applied (color in the below example)
-const Author = ({author}) => {
-    const stlye = {
-        color: "#617d98",
-        fontSize: "0.75rem",
-        marginTop: "0.25rem"
-    }
-    return (
-        <h4 style={stlye}>{author}</h4>
-    )
-}
-
 export default App
-
-// 2:21:45
