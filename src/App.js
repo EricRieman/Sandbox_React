ecbbdafd7c12c24e8e5dfb96ccc5a3df6ed2028e
eric.rieman <eric.rieman@appliedvision.com>
2021-05-27 10:24:50 -0400

    Book data moved to array and mapped to a book list

--- src/App.js
+++ src/App.js
@@ -3,74 +3,62 @@ import React from "react"
 import "./app.css"
 
 function App() {
-    const book1 = {
-        img: "https://images-na.ssl-images-amazon.com/images/I/61pJRtxpm3L._AC_UL210_SR210,210_.jpg",
-        title: "Groundwater Science",
-        author: "Charles R. Fitts"
-    }
-    
-    const book2 = {
-        img: "https://images-na.ssl-images-amazon.com/images/I/51qQ7tba6-L._SX382_BO1,204,203,200_.jpg",
-        title: "Fundamentals of Hydrology",
-        author: "Tim Davie"
-    }
+    const books = [
+        {
+            img: "https://images-na.ssl-images-amazon.com/images/I/61pJRtxpm3L._AC_UL210_SR210,210_.jpg",
+            title: "Groundwater Science",
+            author: "Charles R. Fitts",
+            key: 0
+        },
+        {
+            img: "https://images-na.ssl-images-amazon.com/images/I/51qQ7tba6-L._SX382_BO1,204,203,200_.jpg",
+            title: "Fundamentals of Hydrology",
+            author: "Tim Davie",
+            key: 1
+        }
+    ]
+
+    const mappedBooks = books.map( (book) => <Book data={book} key={book.key}/> )
 
     return (
         <div className = "book-list" >
-            < Book data={book1} />
-            < Book data={book2} />
-            < Book data={book1} />
-            < Book data={book2} />
-
-            <Book Book data={book1}> 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Culpa vitae commodi, eius beatae ut cumque nobis voluptatum 
-                aspernatur dolorem quo molestias eum accusantium corrupti eos 
-                laboriosam. Perspiciatis, ab ut? Fuga.</p>
-            </Book>
+            {mappedBooks}
         </div>       
     )
 }
 
-//instead of using props, we could just list the parameters being passed,
-// removing the need for the props variable
-
-// We can access children elements of the component by passing in the
-// 'children' keyword, as shown below.
-
-const Book = ({data, children}) => {
+const Book = (props) => {
     return (
     <div className = "book">
-        <Image img = {data.img}/>
-        <Title title = {data.title} />
-        <Author author = {data.author} />
-        {children}
+        <Image img = {props.data.img}/>
+        <Title title = {props.data.title} />
+        <Author author = {props.data.author} />
     </div>
     )
 }
 
-const Image = (props) => {
+const Image = ({img}) => {
     return (
-        <img src={props.img} alt=" " />
+        <img src={img} alt=" " />
     )
 }
 
-const Title = (props) => {
+const Title = ({title}) => {
     return (
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
     )
 }
 
 // Note that the JSX styling below will always overwrite whatever is in the .css file,
 // if both style types are applied (color in the below example)
-const Author = (props) => {
+const Author = ({author}) => {
     const stlye = {
         color: "#617d98",
         fontSize: "0.75rem",
         marginTop: "0.25rem"
     }
     return (
-        <h4 style={stlye}>{props.author}</h4>
+        <h4 style={stlye}>{author}</h4>
     )
 }
 
