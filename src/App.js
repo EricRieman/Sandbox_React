5478dbb93ba3e5673b4171d66583feb4dcba14e9
eric.rieman <eric.rieman@appliedvision.com>
2021-06-02 17:01:02 -0400

    Arrays with useState

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {data} from "./data"
 
 const App = () => {
-
-  // useState is a Hook that returns an array containing a value and 
-  // a function  we also pass in a default for the value (in this case, 
-  // the initial text). then, we can use the function to change the
-  // value and react will re-render the page wit the new value.
-
-  // react has a list of Hooks other then useState that all start with 'use'.
-  // To use any hook in your component, the name of the component's first letter 
-  // must start with upper case, which is a general react convention anyways.
-
-  // hooks must be defined inside the component body
-  // hooks cannot be called conditionally
+  //we can either import useState, or access from React like below
+  const [people, setPeople] = React.useState([data])
   
-  const [text, setText] = useState('random title')
-
-  const handleClick = () => {
-    setText('hello world')
+  return ( <> {
+    people.map((person) => {
+      const {id, name} = person
+      return (
+        <div key={id} className="item">
+          <h4>{name}</h4>
+        </div>
+      )
+    })
   }
-
-  return (
-    <div className='container'>
-      <React.Fragment>
-        <h1>{text}</h1>
-        <button className="btn" onClick={handleClick}>
-          Change Title
-        </button>
-      </React.Fragment>
-    </div>
+  </>
   )
 }
 
 export default App;
-
-//use state array: 4:07:15
