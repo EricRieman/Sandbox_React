b733b46644ed8852f865dfb6726f6c066f3b25f6
eric.rieman <eric.rieman@appliedvision.com>
2021-06-03 10:40:34 -0400

    fixed syntax error

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import {data} from "./data"
 
 const App = () => {
   //we can either import useState, or access from React like below
-  const [people, setPeople] = React.useState([data])
-  
+  const [people, setPeople] = React.useState(data)
+
   return ( <> {
     people.map((person) => {
       const {id, name} = person
+      
       return (
         <div key={id} className="item">
           <h4>{name}</h4>
