d5e7057420528336fe50693e699153d164353853
eric.rieman <eric.rieman@appliedvision.com>
2021-06-08 15:55:50 -0400

    Completed bithday reminder proejct

---
+++ Examples/1-BirthdayReminder/final/src/List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const List = (props) => {
+  return (
+    <>
+      {
+        props.people.map( (person) => {
+          const {name, id, age, image} = person
+
+          return (
+            <article key={id} className="person">
+              <img src={image} alt={name}/>
+              <div>
+                <h4>{name}</h4>
+                <p>{age}</p>
+              </div>
+            </article>
+          )
+        })
+      }
+    </>
+  );
+};
+
+export default List;
