77d0be9035eaa56c4b443d7080b82fdaafcff9b1
eric.rieman <eric.rieman@appliedvision.com>
2021-06-08 15:56:45 -0400

    Started learning about useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List'; 
+import React, {useState, useEffect} from 'react';
+
+// by default useEffect runs every re-render
+// it runs the callback function passed in, also known as the side effect.
+// cleanup function
+// second parameter
+
+//rember, we cannot place hooks inside of conditionals, so to selectivly call use state (not run every render)
+//we could put the condition inside of the callback function, but that is not the only solution
+//5:02
 
 const App = () => {
-
-  const [people, setPeople] = setState(data)
-
-  const handleClick = () => {
-    setPeople([])
-  }
-
-  return <main>
-    <section className="container">
-      <h3>0 birthdays today</h3>
-      <List people={people}/>
-      <button onClick={handleClick}>Clear All</button>
-    </section>
-  </main>;
+  const [value, setValue] = useState(0);
+  
+  useEffect(() => {
+    console.log('use effect called')
+    document.title = `New Messages(${value})`
+  })
+  
+  return (
+    <div className ="container">
+      <h1>{value}</h1>
+      <button className='btn' onClick={ () => setValue(value+1)}>Click me</button>
+    </div>
+  )
 }
 
 export default App;
