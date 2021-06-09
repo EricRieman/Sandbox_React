7739266b1253244d89a4d43ff2837282ddc282f5
eric.rieman <eric.rieman@appliedvision.com>
2021-06-09 16:02:50 -0400

    Started learning conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, {useState, useEffect} from 'react';
 
-// by default useEffect runs every re-render
-// it runs the callback function passed in, also known as the side effect.
-// cleanup function
-// second parameter
+// We can conditionally return a different html element from
+// our component
 
-//rember, we cannot place hooks inside of conditionals, so to selectivly call use state (not run every render)
-//we could put the condition inside of the callback function, but that is not the only solution
-//5:02
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const App = () => {
-  const [value, setValue] = useState(0);
-  
-  useEffect(() => {
-    console.log('use effect called')
-    document.title = `New Messages(${value})`
-  })
-  
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  if( isLoading )
+    return (
+      <div className ="container">
+        <p>Loading</p>
+      </div>
+    )
+
   return (
     <div className ="container">
-      <h1>{value}</h1>
-      <button className='btn' onClick={ () => setValue(value+1)}>Click me</button>
+      <p>Not loading</p>
     </div>
   )
 }
