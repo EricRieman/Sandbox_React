7109dd4deff3e69fc02a6bfb43ff4e3d575466c2
Eric Rieman <er28@zips.uakron.edu>
2021-07-04 02:33:42 -0400

    Started working on tours example

--- TutorialExamples/2-Tours/src/App.js
+++ TutorialExamples/2-Tours/src/App.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
-function App() {
-  return <h2>Tours Project Setup</h2>
+
+const App = () => {
+  const [loading, setLoading] = useState( true )
+  const [tours, setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+
+    try {
+      const response = await fetch( url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch(error) {
+      setLoading(false)
+      console.log(error)
+    }
+
+    console.log(tours)
+  }
+
+  useEffect( () => {
+    fetchTours()
+  }, [] )
+
+  return ( <main> {loading ? < Loading /> : <Tours tours = {tours}/>} </main>)
 }
 
 export default App
--- TutorialExamples/2-Tours/src/Tour.js
+++ TutorialExamples/2-Tours/src/Tour.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ( {id, image, info, name, price} ) => {
+  
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name}/>
+      <footer>
+        <div className = 'tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+      </footer>
+    </article>
+  )
 };
+
+//37:00
 
 export default Tour;
