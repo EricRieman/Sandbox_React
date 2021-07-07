242b461b5303a55bc977e110ee2a8cbb57473b39
Eric Rieman <er28@zips.uakron.edu>
2021-07-06 21:56:14 -0400

    completed tours project

--- TutorialExamples/2-Tours/src/App.js
+++ TutorialExamples/2-Tours/src/App.js
@@ -3,17 +3,23 @@ import Loading from './Loading'
 import Tours from './Tours'
 
 const url = 'https://course-api.com/react-tours-project'
+let initialized = false
 
 const App = () => {
   const [loading, setLoading] = useState( true )
   const [tours, setTours] = useState([])
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setLoading(true)
-
     try {
       const response = await fetch( url)
       const tours = await response.json()
+      initialized = true;
       setLoading(false)
       setTours(tours)
     } catch(error) {
@@ -28,7 +34,24 @@ const App = () => {
     fetchTours()
   }, [] )
 
-  return ( <main> {loading ? < Loading /> : <Tours tours = {tours}/>} </main>)
+  if( !initialized )
+    return <></>
+
+  if( tours.length === 0 ) {
+    return (
+      <mian>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={fetchTours}>refresh</button>
+        </div>
+      </mian>
+    )
+  }
+
+  return ( <main> {loading ? < Loading /> : <Tours tours={tours} removeTour={removeTour}/>} </main>)
 }
+
+
+//48:00
 
 export default App
