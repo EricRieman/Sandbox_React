b77a2d13dcdfb9e9cc4141e085dcc7a1132caba3
Eric Rieman <er28@zips.uakron.edu>
2021-07-07 01:11:49 -0400

    Completed review project

--- TutorialExamples/3-Reviews/src/Review.js
+++ TutorialExamples/3-Reviews/src/Review.js
@@ -3,7 +3,43 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index,setIndex] = useState(0);
+  const {name,job,image,text} = people[index]
+
+  const handleNextClick = () => {
+    setIndex( index === people.length - 1 ? 0 : index + 1 )
+  }
+
+  const handlePrevClick = () => {
+    setIndex( index === 0 ? people.length - 1 : index - 1  )
+  }
+
+  const handleRandCick = () => {
+    setIndex(Math.floor( Math.random() * people.length))
+  }
+
+  return (
+    <article className='review'>
+      <div className = 'img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className ='quote-icon'>
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className ='button-container'>
+        <button className='prev-btn' onClick={handlePrevClick}>
+          <FaChevronLeft/> 
+        </button>
+        <button className='next-btn' onClick={handleNextClick}>
+          <FaChevronRight/>
+        </button>
+      </div>
+      <buton className='random-btn' onClick={handleRandCick}>surprise me</buton>
+    </article>
+  )
 };
 
 export default Review;
