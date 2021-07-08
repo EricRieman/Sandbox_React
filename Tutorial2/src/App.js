14c8053ddb6d98c6542aa24c515e3405a57ebd87
eric.rieman <eric.rieman@appliedvision.com>
2021-07-08 15:46:55 -0400

    Setup for prop types

--- Tutorial2/src/App.js
+++ Tutorial2/src/App.js
@@ -1,42 +1,23 @@
-// Prop driling in not an 'official' term, but it is a side effect that occurs 
-// when you have many nested components. You want to keep track of some state
-// in the upper component, and need to pass it down into child components,
-// but it is only being used by the bottom most componenet.
+// Prop types allow us to validate props passed into components.
 
-// There are many ways we can reduce or fix prop drilling - context api, redux
+import React from 'react';
+import {useFetch} from './useFetch'
+import Product from './Product'
 
-import React, {useState} from 'react';
-import {data} from './data'
+const url = 'https://course-api.com/react-prop-types-example'
 
 const App = () => {
+  const {products} = useFetch(url);
 
-  const [people, setPeople] = useState(data)
   return (
-    <section>
-      <h3>prop drilling</h3>
-      <List people={people} />
-    </section>
-  )
-}
-
-const List = ({people}) => {
-  return (
-    <>
-      {people.map((person)=>{
-        return <Person key={person.id} />
-      })}
-    </>
-  )
-}
-
-const Person = ({id, name}) => {
-  return (
-    <div className='item'>
-      <h4>single item</h4>
+    <div>
+      <h2>products</h2>
+      <section className='products'>
+        {products.map( product => { 
+          return <Product/>
+        } )}
+      </section>
     </div>
   )
 }
-
-//8:08
-
 export default App;
---
+++ Tutorial2/src/Product.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Product = () => {
+  return <h2>Testing</h2>
+}
+
+export default Product
