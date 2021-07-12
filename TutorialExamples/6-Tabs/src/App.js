422fdf0ad4f37fe3ee8955be8af37ead69a66e11
Eric Rieman <er28@zips.uakron.edu>
2021-07-12 00:02:10 -0400

    completed tabs project

--- TutorialExamples/6-Tabs/src/App.js
+++ TutorialExamples/6-Tabs/src/App.js
@@ -1,10 +1,70 @@
 import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+import { FaAngleDoubleRight, FaUserTimes } from 'react-icons/fa'
+
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [jobIndex, setJobIndex] = useState(0)
+
+  const fetchJobs = async () => {
+    const res = await fetch(url)
+    const newJobs = await res.json()
+    setJobs(newJobs)
+    setLoading()
+  }
+
+  useEffect( () => {
+    fetchJobs()
+  }, [])
+
+  if( loading )
+    return (
+      <secttion className='section loading'>
+        <h1>loading...</h1>
+      </secttion>
+    )
+
+    // destructuring MUST be done after the loading return, else jobs will be empty
+  const {company, dates, duties, title } = jobs[jobIndex]
+
+  return(
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+
+      <div className = 'jobs-center'>
+        <div className= 'btn-container'>
+          {jobs.map((job, index) => {
+            return (
+            <button 
+              key={job.id} 
+              onClick={()=>setJobIndex(index)}
+              className = {`job-btn ${index === jobIndex && 'active-btn'}`}
+            >
+              {job.company}
+            </button>
+          )})}
+        </div>
+
+        <artcle className = 'job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+            <div key={index} className='job-desc'>
+              <FaAngleDoubleRight className='job-icon'/>
+              <p>{duty}</p>
+            </div>
+          )})}
+        </artcle>
+
+      </div>
+    </section>
+  )
 }
 
 export default App
