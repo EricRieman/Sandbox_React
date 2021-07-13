29619c8e273027fe7d631c5786627aa5760c911a
eric.rieman <eric.rieman@appliedvision.com>
2021-07-13 16:49:36 -0400

    Began react router tutorial

---
+++ Tutorial2/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="" />
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
