f3eb2ce324f7350b17174d325316608d99e0d814
Eric Rieman <er28@zips.uakron.edu>
2021-07-12 22:35:40 -0400

    init tabs project

--- TutorialExamples/7-Slider/src/App.js
+++ TutorialExamples/7-Slider/src/App.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
-function App() {
-  return <h2>slider project setup</h2>;
+
+const App = () => {
+  const [people, setPeople] = useState(data)
+  const [index,setIndex] = useState(0)
+
+  return <section>
+    <div className ='title'>
+      <h2>
+        <span>/</span> reviews
+      </h2>
+    </div>
+  </section>
 }
 
 export default App;
