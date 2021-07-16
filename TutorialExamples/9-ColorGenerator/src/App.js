55df859528309b31474e4f6519c156e1c3ec30f3
Eric Rieman <er28@zips.uakron.edu>
2021-07-16 08:59:30 -0400

    Completed color generator project

--- TutorialExamples/9-ColorGenerator/src/App.js
+++ TutorialExamples/9-ColorGenerator/src/App.js
@@ -3,12 +3,12 @@ import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
-//3:50
+const initialColor = '#f15025'
 
 function App() {
 const [color, setColor] = useState('')
 const [error, setError] = useState(false)
-const [list, setList] = useState(new Values('#f15025').all(10))
+const [list, setList] = useState(new Values(initialColor).all(10))
 
 const handleSubmit = (e) => {
   e.preventDefault()
@@ -17,9 +17,9 @@ const handleSubmit = (e) => {
     setList(colors)
     console.log(colors)
   }
-  catch(error) {
+  catch(err) {
     setError(true)
-    console.log(error)
+    console.log(err)
   }
 }
 
@@ -28,7 +28,13 @@ const handleSubmit = (e) => {
       <section className='container'>
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type='text' value={color} onChange={(e)=> setColor(e.target.value)}/>
+          <input 
+            type='text' 
+            value={color} 
+            onChange={(e)=> setColor(e.target.value)}
+            placeholder={initialColor}
+            className={`${error ? 'error' : null}`}
+          />
           <button type='submit' className='btn'>Generate</button>
         </form>
       </section>
