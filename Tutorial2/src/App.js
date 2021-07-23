2b5cd1c8e2fcffe97a4eccd3b52ef156813345de
eric.rieman <eric.rieman@appliedvision.com>
2021-07-23 15:36:47 -0400

    Added notes  for performance optimization tutorial

--- Tutorial2/src/App.js
+++ Tutorial2/src/App.js
@@ -1,3 +1,9 @@
+// Performace optimization: React.memo, useCallback, useMemo, 
+// Memoizing is a term used when caching results, using memory
+// React is fast by default, optimizations add their own costs
+
+//9:41
+
 const App = () => {
   return (
     <div>
