48c38ebda37ca40d558f2d865c1530cf4175d8d5
eric.rieman <eric.rieman@appliedvision.com>
2021-08-06 16:01:20 -0400

    Started work on the sidebar modal project

--- TutorialExamples/12-SideBarModal/src/App.js
+++ TutorialExamples/12-SideBarModal/src/App.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
+
+//5:28 - refresh on context API
+
 function App() {
   return (
     <>
-      <h2>modal-sidebar project setup</h2>
+      <Home />
+      <Modal />
+      <Sidebar />
     </>
   )
 }
--- TutorialExamples/12-SideBarModal/src/Sidebar.js
+++ TutorialExamples/12-SideBarModal/src/Sidebar.js
@@ -2,9 +2,36 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { icons } from 'react-icons/lib'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return <aside className={`sidebar`}>
+    <div className="sidebar-header">
+      <img src={logo} alt='n/a' className='logo'/>
+      <button className='close-btn'>
+        <FaTimes/>
+      </button>
+    </div>
+    <ul className='links'>
+      {links.map((link) => {
+        return <li key={link.id}>
+          <a href={link.url}>
+            {link.icon}
+            {link.text}
+          </a>
+        </li>
+      })}
+    </ul>
+    <ul className='social-icons'>
+      {social.map((link) =>{
+        return <li key={link.id}>
+          <a href={link.url}>
+            {link.icon}
+          </a>
+        </li>
+      })}
+    </ul>
+  </aside>
 }
 
 export default Sidebar
