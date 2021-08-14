f05637cf505f8ff7590ccbace4aabca3322ba1cb
Eric Rieman <er28@zips.uakron.edu>
2021-08-14 01:20:02 -0400

    Removed props from MeetupItem component

--- NextJs/nextjs-course/components/meetups/MeetupItem.js
+++ NextJs/nextjs-course/components/meetups/MeetupItem.js
@@ -3,24 +3,24 @@ import {useRouter} from 'next/router'
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
-function MeetupItem(props) {
+function MeetupItem({id, image, title, address}) {
   const router = useRouter()
 
   const showDetailsHandler = () => {
     //We could also define an anchor or <Link>, but with the router object, we
     // can also use this push method, which is convenient for button handlers.
-    router.push('/' + props.id)
+    router.push('/' + id)
   }
 
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <img src={image} alt={title} />
         </div>
         <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
+          <h3>{title}</h3>
+          <address>{address}</address>
         </div>
         <div className={classes.actions}>
           <button onClick={showDetailsHandler}>Show Details</button>
